3e6e72aa7fcdc9a778b1759cb3730db93c84a521
Zach <104291766+zachary-o@users.noreply.github.com>
2023-03-05 21:20:52 +0200

    add theme mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import { createContext, useState } from "react";
+
+import Switch from "react-switch";
+
 import { Header } from "./assets/components/Header";
 import { Meme } from "./assets/components/Meme";
 
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, seTheme] = useState("light");
+
+  const toggleTheme = () => {
+    seTheme((current) => (current === "light" ? "dark" : "light"));
+  };
 
   return (
-    <div className="container">
-      <Header />
-      <Meme />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="container" id={theme}>
+        <Header />
+        <Meme />
+        <div className="switch">
+          <Switch onChange={toggleTheme} checked={theme === "dark"}/>
+        </div>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
